b2833ff27174e9701b28b1cd92729ddabf379cd7
Mehmet Emin Eker <mehmetemineker92@gmail.com>
2023-03-03 09:09:26 +0300

    "the lord of the rings" apps created💍

---
+++ packages/book-chapter-list/src/App.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>Book Chapter List Micro Frontend</div>
+</template>
---
+++ packages/book-list/src/App.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>Book List Micro Frontend</div>
+</template>
